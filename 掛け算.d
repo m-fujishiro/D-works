d1f4a671f3c48e29dc9c6d0284bffe5d19252f6c
m.f <44862768+m-fujishiro@users.noreply.github.com>
2018-11-09 16:25:02 +0900

    Add files via upload

---
+++ 掛け算.d
@@ -0,0 +1,32 @@
+
+
+import std.stdio;
+
+import std.string;
+
+import std.conv;
+
+
+void main(){
+
+    string a,b;
+
+    //標準入力を一行読み込む*2
+    a = readln();
+
+    b = readln();
+
+    //最後の改行コードを消す
+    a = chomp(a).dup;
+
+    b = chomp(b).dup;
+    //整数型に変換する
+
+    int x = to!(int)(a);
+
+    int y = to!(int)(b);
+
+    
+writeln(x*y);
+
+}
---
+++ 足し算.d
@@ -0,0 +1,13 @@
+import std.stdio;
+import std.conv;
+import std.array;
+
+void main(){
+    //ˆês“Ç‚İ‚ñ‚Å‹ó”’‚Å‹æØ‚Á‚Ä”z—ñ‚ÉŠi”[
+    string[] n = split(readln());
+    //a‚Æb‚ğ”’l‚É•ÏŠ·
+    int a = to!int(n[0]),b = to!int(n[1]);
+    
+    writeln(a+b);
+    
+}
